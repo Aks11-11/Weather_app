8450e551570ca1e3bae81838f27658fc4fa8ebec
Aks11-11 <aksshatvashisht@gmail.com>
2024-03-20 23:44:24 +0530

    Final1

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ def getWeather():
         current_time=local_time.strftime("%I:%M %p")
         clock.config(text= current_time)
         name.config(text ="CURRENT WEATHER")
-
         #weather
         api = "https:// your api source"+city+"&Appid = APikey"
         json_data=requests.get(api).json()
@@ -60,25 +59,21 @@ textfield.focus()
 Search_icon = PhotoImage(file = "/Users/akshat/Desktop/icons/Copy of search_icon.png")
 myimage_icon = Button(image = Search_icon,borderwidth=0,cursor="hand2",bg = "#404040",command= getWeather)
 myimage_icon.place(x=400,y=34)
-
  #logo
 
 Logo_image = PhotoImage(file = "/Users/akshat/Desktop/icons/Copy of logo.png")
 logo = Label(image=Logo_image)
 logo.place(x=150,y=80)
-
 #Bottom Box
 Frame_image = PhotoImage(file = "/Users/akshat/Desktop/icons/Copy of box.png")
 frame_myimage = Label(image = Frame_image)
 frame_myimage.pack(padx=5,pady=5,side=BOTTOM)
-
 #time
 
 name = Label(root,font=("arial",15,"bold"))
 name.place(x=30,y=100)
 clock = Label(root,font=("Helvetica",20))
 clock.place(x=30,y=130)
-
 #label
 
 label1 = Label(root,text="WIND",font=("Helvetica",15,'bold'),fg ="white",bg ="#1ab5ef" )
